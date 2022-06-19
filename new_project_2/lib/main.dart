b7d75afab81c6a1c95a1aac5c2d59dc5ebb14ab2
sanksarraval <82793958+sanksarraval@users.noreply.github.com>
2022-06-19 01:04:59 -0500

    Adding comments

--- new_project_2/lib/main.dart
+++ new_project_2/lib/main.dart
@@ -1,11 +1,17 @@
+// Imports the package required to run it with the fluttrer app.
 import 'package:flutter/material.dart';
 void main()
 {
-  runApp(MyApp());
+  // Run app is a function which does all the heavu lifting, it takes a widget and draws it onto the screen.
+  // It calls the build method.
+  runApp(MyApp()); // Creating an object of MyApp class and then pass it on to flutter.
 }
+// Shorthand for any function with one expression.
+//void main() => runApp(MyApp())
 
+// Responsible for returning a new widget to draw on the screen.
 class MyApp extends StatelessWidget {
-  @override
+  @override // Decorator provided by dart. It is just there to make the code cleaner and clearer.
   Widget build(BuildContext context) {
     return MaterialApp(home: Text('Hello!'),);
   }
