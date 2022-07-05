24b3cb64aee81da6b4cc39cc879323e27f1bd252
sanksarraval <82793958+sanksarraval@users.noreply.github.com>
2022-07-05 14:25:00 -0500

    Update question.dart

--- new_project_2/lib/question.dart
+++ new_project_2/lib/question.dart
@@ -7,8 +7,9 @@ class Question extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
+    return Container( // Container is a widget. 
+      width: double.infinity, // it takes the full width of the screen.
+      margin: EdgeInsets.all(10), // It takes the margin of the container.
       child: Text(
       questionText,
       style: TextStyle(fontSize: 28),
